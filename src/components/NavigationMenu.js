643a2ce7b964c36f474542508a7c17bad9f849f4
Josh Bickett <josh@bickett.net>
2022-02-27 15:59:49 -0800

    Add UI reativity

--- src/components/NavigationMenu.js
+++ src/components/NavigationMenu.js
@@ -1,11 +1,15 @@
 import styled from "@emotion/styled";
 
 const NavigationMenu = () => {
+  // https://www.videezy.com/music-related/41219-music-equalizer-for-music-party
+  // https://medium.com/geekculture/6-top-popular-react-js-animation-libraries-in-2021-e6ea75bb6fcd
   return (
     <Container>
-      <Brand>Makesong.ai</Brand>
-      <NavigationItem>About the project</NavigationItem>
+      <Brand>makesong.ai</Brand>
+      <NavigationItem>The Technology</NavigationItem>
       <NavigationItem>Join waitlist</NavigationItem>
+      <NavigationItem>The App</NavigationItem>
+      <AnimationContainer>COMING SOON</AnimationContainer>
     </Container>
   );
 };
@@ -16,7 +20,7 @@ const Container = styled.div`
   margin: 0;
   width: 100%;
   min-height: 20px;
-  border: 1px solid white;
+  border-bottom: 1px solid white;
 `;
 
 const Brand = styled.div`
@@ -25,8 +29,26 @@ const Brand = styled.div`
 `;
 
 const NavigationItem = styled.div`
-  font-size: 35px;
+  font-size: 30px;
   border: 1px solid white;
-  width: 20%;
+  width: 350px;
   float: right;
+  &:hover {
+    background-color: #f0c;
+    color: #fff;
+  }
+  &:active {
+    background-color: #03f;
+    color: #f90;
+  }
+`;
+
+const AnimationContainer = styled.div`
+  margin: 0;
+  font-size: 30px;
+  position: absolute;
+  width: 100%;
+  top: 50%;
+  -ms-transform: translateY(-50%);
+  transform: translateY(-50%);
 `;
