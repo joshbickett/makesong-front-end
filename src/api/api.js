8ce24bd1f93cc3b245d7ece717ed72f37be62999
Josh Bickett <josh@bickett.net>
2022-06-05 20:25:35 -0700

    Add prod endpoint

--- src/api/api.js
+++ src/api/api.js
@@ -1,3 +1,9 @@
+// test
+// const url = "http://127.0.0.1:5000";
+
+// prod
+const url = "http://makesong.ai:5000";
+
 export const saveEmail = async (email) => {
   var formdata = new FormData();
   formdata.append("email", email);
@@ -11,7 +17,7 @@ export const saveEmail = async (email) => {
     },
   };
 
-  const result = await fetch("http://127.0.0.1:5000/save_email", request)
+  const result = await fetch(url + "/save_email", request)
     .then((response) => response.json())
     .catch((e) => {
       console.log("there was an error: ", e);
