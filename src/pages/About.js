cbb203519e6100d6e71b1183ba0e4204e8b60c30
Josh Bickett <josh@bickett.net>
2022-07-28 20:35:55 -0700

    Add some about info

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,20 +5,27 @@ const About = () => {
   return (
     <Container>
       <NavigationMenu currentPage={"About"} />
-      <div class="fluid-container">
-        <div class="row">
-          <div class="col-md-3"></div>
-          <div class="col-md-6">
-            <AnimationContainer>
-              <video width="400" height="300" autoPlay loop muted>
-                <source
-                  type="video/mp4"
-                  src="https://static.videezy.com/system/resources/previews/000/041/236/original/84.Music-note-equalizer.mp4"
-                />
-              </video>
-            </AnimationContainer>
-          </div>
-          <div class="col-md-3"></div>
+
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <div>
+          <Title>
+            Makesong.ai is a fork of{" "}
+            <a href="https://github.com/openai/jukebox/">Jukebox</a>
+          </Title>
+          <AnimationContainer>
+            <video width="400" height="300" autoPlay loop muted>
+              <source
+                type="video/mp4"
+                src="https://static.videezy.com/system/resources/previews/000/041/236/original/84.Music-note-equalizer.mp4"
+              />
+            </video>
+          </AnimationContainer>
         </div>
       </div>
 
@@ -56,7 +63,7 @@ const Container = styled.div`
 `;
 
 const AnimationContainer = styled.div`
-  margin-top: 250px;
+  margin-top: 150px;
   font-size: 30px;
   top: 60%;
   -ms-transform: translateY(-50%);
@@ -66,4 +73,8 @@ const AnimationContainer = styled.div`
 const Attribution = styled.div`
   font-size: 10px;
   margin: 10px;
+`;
+
+const Title = styled.h2`
+  margin-top: 150px;
 `;
